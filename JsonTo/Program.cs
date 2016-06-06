41d539df2e856f225683916461a67d9603d2360e
Jason Romero <codenamejason@outlook.com>
2016-06-06 10:38:36 -0400

    Initial Commit

--- JsonTo/Program.cs
+++ JsonTo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,21 @@ namespace JsonTo
     {
         static void Main(string[] args)
         {
+            string email = "";
+            string json = "{\"test\":true,\"test2\":false}";
+            using (var client = new HttpClient())
+            {
+                string url = "https://json-csv.com/api/getcsv";
+                var content = new FormUrlEncodedContent(new[]
+                {
+            new System.Collections.Generic.KeyValuePair("email", email),
+            new System.Collections.Generic.KeyValuePair("json", json),
+        });
+                var result = client.PostAsync(url, content).Result;
+                string csvContent = result.Content.ReadAsStringAsync().Result;
+                Response.Write(csvContent);
+            }
+
         }
     }
 }
